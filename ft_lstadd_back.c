aa04205ccb9064c1cd96fab5b245377c92c55ba7
kapinarc <kaankorbax@gmail.com>
2024-11-12 13:02:22 +0100

    add ex ft_lstadd_back n ft_lstdelone.c

---
+++ ft_lstadd_back.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kapinarc <kapinarc@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 11:42:37 by kapinarc          #+#    #+#             */
+/*   Updated: 2024/11/12 11:42:37 by kapinarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (lst)
+	{
+		if (!*lst)
+			*lst = new;
+		else
+		{
+			last = ft_lstlast(*lst);
+			last->next = new;
+		}
+	}
+}
+/*
+int	main(void)
+{
+
+	t_list *list = NULL;
+	t_list *lst1 = ft_lstnew("Hello");
+	t_list *lst2 = ft_lstnew("World");
+	t_list *lst3 = ft_lstnew("!");
+	// Ajouter le premier élément
+	ft_lstadd_back(&list, lst1);
+	// Ajouter un deuxièmelst
+	ft_lstadd_back(&list, lst2);
+	// Ajouter un troisièmlstt
+	ft_lstadd_back(&list, lst3);
+	// Afficher le contenu de la liste
+	t_list *current = list;
+	while (current)
+	{
+		printf("%s\n", (char *)current->content);
+		current = current->next;
+	}
+	// Libérer la mémoire allouée
+	current = list;
+	while (current)
+	{
+		t_list *temp = current;
+		current = current->next;
+		free(temp);
+	}
+		return 0;
+}
+*/
